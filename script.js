3798f9dfb8e9c82902a1aea18377cb415a2cffa9
dpk5e7 <dpk5e7@yahoo.com>
2022-06-30 15:59:03 -0600

    Added logic to prompt user for input and to create a random password based on that user input

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -16,5 +14,42 @@ generateBtn.addEventListener("click", writePassword);
 
 // My code goes here.
 function generatePassword() {
-  return "Test";
+
+  let newPassword = "";
+  let passwordLength = parseInt(prompt("How many characters would you like your password to be?\nPlease enter a number at least 8 characters long and no more than 128 characters."));
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    let includeLowercase = confirm("Would you like to include lowercase letters?\nPress OK for Yes and Cancel for No.");
+    let includeUppercase = confirm("Would you like to include uppercase letters?\nPress OK for Yes and Cancel for No.");
+    let includeNumbers = confirm("Would you like to include numbers?\nPress OK for Yes and Cancel for No.");
+    let includeSpecial = confirm("Would you like to include special characters?\nPress OK for Yes and Cancel for No.");
+
+    if (includeLowercase || includeUppercase || includeNumbers || includeSpecial) {
+      let possibleCharacters = "";
+      if (includeLowercase) {
+        possibleCharacters += "abcdefghijklmnopqrstuvwxyz";
+      }
+      if (includeUppercase) {
+        possibleCharacters += "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+      }
+      if (includeNumbers) {
+        possibleCharacters += "0123456789";
+      }
+      if (includeSpecial) {
+        possibleCharacters += "!@#$%^&*()";
+      }
+      
+      for (let i = 0; i <= passwordLength; i++) {
+        newPassword += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+      }
+    }
+    else {
+      alert("You have to pick at least one type of character.\nPlease try again.");
+    }
+  }
+  else {
+    alert("You did not enter a valid length.\nPlease try again.");
+  }
+
+  return newPassword;
 }
