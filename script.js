b455ce18c5bdeda1b9214c362984747981092e90
dpk5e7 <dpk5e7@yahoo.com>
2022-06-29 22:25:29 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// My code goes here.
+function generatePassword() {
+  return "Test";
+}
